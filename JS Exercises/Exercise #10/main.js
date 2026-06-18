cea6de55ed5b2d7e0674b0b030a5a61de52dfbaf
absherGitHup <absherissa@gmail.com>
2026-06-18 17:22:15 +0300

    Exercise #10

---
+++ JS Exercises/Exercise #10/main.js
@@ -0,0 +1,17 @@
+let library = [
+    {
+        title : "48 Laws of Power",
+        author: "John M.",
+        year : 1990
+    },
+    {
+        title : "Atomic Habit",
+        author: "Jack M.",
+        year : 2010
+    }
+    
+]
+console.log(library[0].title);
+console.log(library[1].author);
+
+
