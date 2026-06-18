e0cd0b8baa068c474f0a70e95de9efb943b04dd6
absherGitHup <absherissa@gmail.com>
2026-06-18 17:10:57 +0300

    Exercise #9

---
+++ JS Exercises/Exercise #9/main.js
@@ -0,0 +1,11 @@
+let car = {
+    make : "2020",
+    model : "MARCEDES",
+    year : "2025",
+    start : function(){
+        console.log("The car has Started");
+        
+    }
+}
+
+console.log(car.start());
