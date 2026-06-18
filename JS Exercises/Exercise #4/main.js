37745a59d0f08f36ca451e8e1bbac45e9d99547c
absherGitHup <absherissa@gmail.com>
2026-06-18 15:22:53 +0300

    Excercise #4

---
+++ JS Exercises/Exercise #4/main.js
@@ -0,0 +1,22 @@
+
+// Question one
+function add(a,b){
+    console.log(a + b);
+}
+add(5,19);
+add(1,2);
+add(1,6);
+add(2,2);
+add(23,12);
+
+// Question two
+
+const expressionFunction = function(a,b){
+    return a + b;
+}
+
+console.log(expressionFunction(10,30));
+console.log(expressionFunction(30,50));
+console.log(expressionFunction(40,70));
+console.log(expressionFunction(50,80));
+console.log(expressionFunction(60,90));
