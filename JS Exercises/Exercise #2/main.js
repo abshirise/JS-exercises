9526e8d3d0c3c17b0c570b69598e20fba9ea7702
absherGitHup <absherissa@gmail.com>
2026-06-18 14:20:15 +0300

    Exercise #2

---
+++ JS Exercises/Exercise #2/main.js
@@ -0,0 +1,13 @@
+let a = 15
+let b = "15";
+
+console.log(a == b); // equal to
+console.log(a != b); // not equal to
+console.log(a === b); // strictly equal to
+console.log(a !== b); // strictly not equal to
+console.log(a > b); // greater than 
+console.log(a >= b); // greater than or equal to
+console.log(a < b); // less than
+console.log(a <= b); // less than or equal to
+
+
