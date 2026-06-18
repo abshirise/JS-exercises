edcbf9310b5be4747a589a1a13865ca406d8f0ef
absherGitHup <absherissa@gmail.com>
2026-06-18 13:47:18 +0300

    Excercise #1

---
+++ Exercise #1/main.js
@@ -0,0 +1,11 @@
+// creating variables 
+let x = 7 
+let y = 3
+
+console.log(x + y); // adding
+console.log(x - y); // subtraction
+console.log(x * y); // multiplication
+console.log(x / y); // division
+console.log(x % y); // modulus 
+console.log(x ++); // increment 
+console.log(-- y); // decrement 
