256da4a9a60d2cec9dcf4e8f3b04ae19ea08af5b
absherGitHup <absherissa@gmail.com>
2026-06-18 15:28:44 +0300

    Exercise #5

---
+++ JS Exercises/Exercise #5/main.js
@@ -0,0 +1,11 @@
+// arrow function
+
+const add = (a,b) => {
+    return a + b;
+}
+
+console.log(add(1,5));
+console.log(add(2,4));
+console.log(add(3,3));
+console.log(add(4,2));
+console.log(add(5,1));
