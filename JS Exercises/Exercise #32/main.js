91bcfb82f852462de960eada6491e47eaf239399
absherGitHup <absherissa@gmail.com>
2026-06-27 14:05:54 +0300

    Exercise #32

---
+++ JS Exercises/Exercise #32/main.js
@@ -0,0 +1,19 @@
+
+
+const items = document.querySelectorAll('.item')
+console.log(items);
+
+const item1 = document.querySelector('#item1')
+console.log(item1);
+
+const item2 = document.querySelector('#item2')
+console.log(item2);
+
+const item3 = document.querySelector('#item3')
+console.log(item3);
+
+const item4 = document.querySelector('#item4')
+console.log(item4);
+
+
+
